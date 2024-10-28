d3f31d83fc32c2e52dec957d0249c341a9e46cb8
MGausseran <martin.gausseran@gmail.com>
2024-10-28 15:14:05 +0100

    Ajout du header

---
+++ vocalease/src/components/header.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './header.css';
+
+const Header = () => {
+  // État pour gérer l'ouverture/fermeture du menu
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Fonction pour basculer l'état du menu
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="header">
+      <h1>VocalEase</h1>
+      <div className="menu-icon" onClick={toggleMenu}>
+        &#9776; {/* Icône hamburger */}
+      </div>
+
+      {/* Menu déroulant */}
+      <nav className={`dropdown-menu ${isOpen ? 'open' : ''}`}>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">Settings</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
