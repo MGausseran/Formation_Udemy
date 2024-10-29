6698ce1db6f0f24580503eca448a8f51998dd653
Martin Gausseran <martin.gausseran@gmail.com>
2024-10-29 12:23:44 +0100

    Ajout de quelques éléments HTML/CSS + Reconfiguration sur le PC fixe

--- vocalease/src/pages/homepage.js
+++ vocalease/src/pages/homepage.js
@@ -9,8 +9,22 @@ const Homepage = () => {
     <div className="homepage">
       <div className="body">
         <h2>Bienvenue sur VocalEase !</h2>
-        <p>C'est ici que vous pouvez perfectionner votre prononciation.</p>
-        <button>A vous de jouer !</button>
+        <p>C'est ici que vous allez perfectionner votre prononciation.</p>
+        <section className='main_menu'>
+          <h3 className='main_menu_title'>Alors, on apprend quoi aujourd'hui ?</h3>
+          <h4 className='sub_menus start'>Découvrir</h4>
+          <ul className='languages_list'>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <h4 className='sub_menus reload'>Reprendre</h4>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </section>
       </div>
     </div>
     </div>
